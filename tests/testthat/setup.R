36eba28012527d5de47ed44d84aa50a40beb2098
Sam <s.e.abbott12@gmail.com>
2024-08-29 20:51:32 +0100

    overall ci and unit tests for expgrowth

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -2,7 +2,9 @@
 options(datatable.print.class = FALSE)
 options(datatable.print.keys = FALSE)
 
-if (on_ci() && Sys.info()["sysname"] == "Linux" && not_on_cran()) {
+if (
+  !on_ci() || (on_ci() && Sys.info()["sysname"] == "Linux" && not_on_cran())
+) {
   library(cmdstanr)
   stan_functions <- pcd_load_stan_functions(
     wrap_in_block = TRUE,
@@ -12,4 +14,5 @@ if (on_ci() && Sys.info()["sysname"] == "Linux" && not_on_cran()) {
   model <- suppressMessages(suppressWarnings(cmdstanr::cmdstan_model(
     file.path(tempdir(), "pcd_stan_functions.stan")
   )))
+  model$expose_functions(global = TRUE)
 }
---
+++ tests/testthat/test-stan-expgrowth.R
@@ -0,0 +1,67 @@
+skip_on_cran()
+skip_on_os("windows")
+skip_on_os("mac")
+
+test_that("Stan expgrowth_pdf matches R dexpgrowth", {
+  x <- seq(0, 1, by = 0.1)
+  min <- 0
+  max <- 1
+  r <- 0.5
+
+  stan_pdf <- sapply(x, expgrowth_pdf, min, max, r)
+  r_pdf <- dexpgrowth(x, min, max, r)
+
+  expect_equal(stan_pdf, r_pdf, tolerance = 1e-4)
+})
+
+test_that("Stan expgrowth_lpdf matches R dexpgrowth with log = TRUE", {
+  x <- seq(0, 1, by = 0.1)
+  min <- 0
+  max <- 1
+  r <- 0.5
+
+  stan_lpdf <- sapply(x, expgrowth_lpdf, min, max, r)
+  r_lpdf <- dexpgrowth(x, min, max, r, log = TRUE)
+
+  expect_equal(stan_lpdf, r_lpdf, tolerance = 1e-4)
+})
+
+test_that("Stan expgrowth_cdf matches R pexpgrowth", {
+  x <- seq(0, 1, by = 0.1)
+  min <- 0
+  max <- 1
+  r <- 0.5
+
+  stan_cdf <- sapply(x, expgrowth_cdf, min, max, r)
+  r_cdf <- pexpgrowth(x, min, max, r)
+
+  expect_equal(stan_cdf, r_cdf, tolerance = 1e-4)
+})
+
+test_that("Stan expgrowth_lcdf matches R pexpgrowth with log.p = TRUE", {
+  x <- seq(0, 1, by = 0.1)
+  min <- 0
+  max <- 1
+  r <- 0.5
+
+  stan_lcdf <- sapply(x, expgrowth_lcdf, min, max, r)
+  r_lcdf <- pexpgrowth(x, min, max, r, log.p = TRUE)
+
+  expect_equal(stan_lcdf, r_lcdf, tolerance = 1e-6)
+})
+
+test_that("Stan expgrowth_rng matches distribution of R rexpgrowth", {
+  n <- 10000
+  min <- 0
+  max <- 1
+  r <- 0.5
+
+  set.seed(123)
+  stan_samples <- replicate(n, expgrowth_rng(min, max, r))
+  set.seed(123)
+  r_samples <- rexpgrowth(n, min, max, r)
+
+  expect_equal(mean(stan_samples), mean(r_samples), tolerance = 1e-2)
+  expect_equal(sd(stan_samples), sd(r_samples), tolerance = 1e-2)
+  expect_equal(quantile(stan_samples), quantile(r_samples), tolerance = 1e-2)
+})
