582c0e613abd7a36ed9f0d38ca17d2ec6b6bedfa
Sam <s.e.abbott12@gmail.com>
2024-09-09 21:26:47 +0100

    first pass at fitdistdoublecens

--- R/fitdistdoublecens.R
+++ R/fitdistdoublecens.R
@@ -4,28 +4,27 @@
 #' censored data using fitdistrplus and primarycensoreddist.
 #'
 #' @param censdata A data frame with columns 'left' and 'right' representing
-#'   the lower and upper bounds of the censored observations.
+#'   the lower and upper bounds of the censored observations. Unlike
+#' [fitdistrplus::fitdistcens()] `NA` is not supported for either the
+#' upper or lower bounds.
+#'
 #' @param distr A character string naming the distribution to be fitted.
-#' @param start A named list giving the initial values for the parameters of
-#'   the named distribution.
-#' @param pwindow Numeric, primary event window (default: 1).
-#' @param D Numeric, maximum delay for truncation (default: Inf).
-#' @param dprimary Function for the primary event distribution
-#'   (default: stats::dunif).
-#' @param dprimary_args List of additional arguments for dprimary.
-#' @param ... Additional arguments to be passed to fitdist.
+#'
+#' @inheritParams pprimarycensoreddist
+#'
+#' @param ... Additional arguments to be passed to [fitdistrplus::fitdist()].
 #'
 #' @return An object of class "fitdist" as returned by fitdistrplus::fitdist.
-#'
-#' @importFrom stats get
 #'
 #' @export
 #'
 #' @examples
-#' # Example usage:
-#' # censdata <- data.frame(left = c(1, 2, 3), right = c(2, 3, 4))
-#' # fit <- fitdistdoublecens(censdata, "gamma", start = list(shape = 1, rate = 1))
-fitdistdoublecens <- function(censdata, distr, start,
+#' censdata <- data.frame(left = c(1, 2, 3), right = c(2, 3, 4))
+#' fit <- fitdistdoublecens(
+#'   censdata, "gamma",
+#'   start = list(shape = 1, rate = 1)
+#' )
+fitdistdoublecens <- function(censdata, distr,
                               pwindow = 1, D = Inf,
                               dprimary = stats::dunif,
                               dprimary_args = list(), ...) {
@@ -36,7 +35,6 @@ fitdistdoublecens <- function(censdata, distr, start,
     )
   }
 
-  # Validate input
   if (!all(c("left", "right") %in% names(censdata))) {
     stop("censdata must contain 'left' and 'right' columns")
   }
@@ -44,55 +42,58 @@ fitdistdoublecens <- function(censdata, distr, start,
   # Get the distribution functions
   pdist <- get(paste0("p", distr))
 
-  # Calculate unique secondary intervals and their frequencies
   swindows <- censdata$right - censdata$left
-  unique_swindows <- unique(swindows)
-  swindow_freq <- table(swindows)
+
+  assign(paste0("dpcens_", distr), function(x, ...) {
+    .dpcens(
+      x, swindows, pdist, pwindow, D, dprimary, dprimary_args, ...
+    )
+  })
+
+  assign(paste0("ppcens_", distr), function(q, ...) {
+    .ppcens(q, pdist, pwindow, D, dprimary, dprimary_args, ...)
+  })
+
 
   # Fit the distribution
   fit <- fitdistrplus::fitdist(
     censdata$left,
     distr = paste0("pcens_", distr),
-    start = start,
-    custom.family = list(
-      dname = paste0("dpcens_", distr),
-      pname = paste0("ppcens_", distr)
-    ),
-    densfun = function(x, ...) dpcens(x, pdist, pwindow, D, dprimary, dprimary_args, unique_swindows, swindow_freq, ...),
-    probfun = function(q, ...) ppcens_(q, pdist, pwindow, D, dprimary, dprimary_args, ...),
     ...
   )
 
   return(fit)
 }
 
-#' Define custom density function
+#' Define a fitdistrplus compatible wrapper around dprimarycensoreddist
+#' @inheritParams dprimarycensoreddist
+#'
+#' @param swindows A numeric vector of secondary window sizes corresponding to
+#' each element in x
 #' @keywords internal
-dpcens <- function(x, pdist, pwindow, D, dprimary, dprimary_args, unique_swindows, swindow_freq, ...) {
-  result <- tryCatch(
+.dpcens <- function(x, swindows, pdist, pwindow, D, dprimary,
+                    dprimary_args, ...) {
+  tryCatch(
     {
-      densities <- vapply(unique_swindows, function(sw) {
+      vapply(seq_along(x), function(i) {
         dprimarycensoreddist(
-          x, pdist,
-          pwindow = pwindow, swindow = sw,
-          D = D, dprimary = dprimary, dprimary_args = dprimary_args, ...
+          x[i], pdist,
+          pwindow = pwindow, swindow = swindows[i], D = D, dprimary = dprimary,
+          dprimary_args = dprimary_args, ...
         )
       }, numeric(length(x)))
-
-      # Scale densities by frequency and sum
-      rowSums(densities * rep(swindow_freq, each = length(x)))
     },
     error = function(e) {
       rep(NaN, length(x))
     }
   )
-  return(result)
 }
 
-#' Define custom distribution function
+#' Define a fitdistrplus compatible wrapper around pprimarycensoreddist
+#' @inheritParams pprimarycensoreddist
 #' @keywords internal
-ppcens_ <- function(q, pdist, pwindow, D, dprimary, dprimary_args, ...) {
-  result <- tryCatch(
+.ppcens <- function(q, pdist, pwindow, D, dprimary, dprimary_args, ...) {
+  tryCatch(
     {
       pprimarycensoreddist(
         q, pdist,
@@ -104,5 +105,4 @@ ppcens_ <- function(q, pdist, pwindow, D, dprimary, dprimary_args, ...) {
       rep(NaN, length(q))
     }
   )
-  return(result)
 }
