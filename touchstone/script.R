ce9c8af7f26050e0b4f28987cc4fb56dd736d52c
Sam <s.e.abbott12@gmail.com>
2024-09-17 16:54:30 +0100

    move setup.R content into main script

--- touchstone/script.R
+++ touchstone/script.R
@@ -130,11 +130,54 @@ touchstone::benchmark_run(
   n = 10
 )
 
-
 # Benchmark for fitting lognormal distribution
 touchstone::benchmark_run(
   expr_before_benchmark = {
-    source("touchstone/setup.R")
+    library(primarycensoreddist)
+    library(dplyr)
+    library(cmdstanr)
+
+    cmdstanr::set_cmdstan_path()
+    options(mc.cores = 2)
+
+    set.seed(123)
+    n1 <- 2000
+    true_meanlog1 <- 1.5
+    true_sdlog1 <- 0.5
+
+    simulated_delays1 <- rprimarycensoreddist(
+      n = n1,
+      rdist = rlnorm,
+      meanlog = true_meanlog1,
+      sdlog = true_sdlog1,
+      pwindow = 1,
+      D = 10
+    )
+
+    example_data1 <- data.frame(
+      delay = simulated_delays1,
+      delay_upper = simulated_delays1 + 1,
+      pwindow = 1,
+      relative_obs_time = 10
+    )
+
+    delay_counts1 <- example_data1 |>
+      summarise(
+        n = n(),
+        .by = c(pwindow, relative_obs_time, delay, delay_upper)
+      )
+
+    stan_data1 <- pcd_as_cmdstan_data(
+      delay_counts1,
+      dist_id = 1,
+      primary_dist_id = 1,
+      param_bounds = list(lower = c(-Inf, 0), upper = c(Inf, Inf)),
+      primary_param_bounds = list(lower = numeric(0), upper = numeric(0)),
+      priors = list(location = c(0, 1), scale = c(1, 1)),
+      primary_priors = list(location = numeric(0), scale = numeric(0))
+    )
+
+    model <- suppressMessages(suppressWarnings(pcd_cmdstan_model()))
   },
   cmdstan_fit_lognormal = {
     fit1 <- model$sample(
@@ -154,7 +197,54 @@ touchstone::benchmark_run(
 # Benchmark for fitting gamma distribution
 touchstone::benchmark_run(
   expr_before_benchmark = {
-    source("touchstone/setup.R")
+    library(primarycensoreddist)
+    library(dplyr)
+    library(cmdstanr)
+
+    cmdstanr::set_cmdstan_path()
+    options(mc.cores = 2)
+
+    set.seed(456)
+    n2 <- 2000
+    true_shape2 <- 2
+    true_rate2 <- 0.5
+
+    simulated_delays2 <- rprimarycensoreddist(
+      n = n2,
+      rdist = rgamma,
+      shape = true_shape2,
+      rate = true_rate2,
+      pwindow = 2,
+      swindow = 2,
+      D = 8,
+      rprimary = rexpgrowth,
+      rprimary_args = list(r = 0.1)
+    )
+
+    example_data2 <- data.frame(
+      delay = simulated_delays2,
+      delay_upper = simulated_delays2 + 2,
+      pwindow = 2,
+      relative_obs_time = 8
+    )
+
+    delay_counts2 <- example_data2 |>
+      summarise(
+        n = n(),
+        .by = c(pwindow, relative_obs_time, delay, delay_upper)
+      )
+
+    stan_data2 <- pcd_as_cmdstan_data(
+      delay_counts2,
+      dist_id = 2,
+      primary_dist_id = 2,
+      param_bounds = list(lower = c(0, 0), upper = c(Inf, Inf)),
+      primary_param_bounds = list(lower = 0, upper = Inf),
+      priors = list(location = c(2, 1), scale = c(0.5, 0.5)),
+      primary_priors = list(location = 0.1, scale = 0.1)
+    )
+
+    model <- suppressMessages(suppressWarnings(pcd_cmdstan_model()))
   },
   cmdstan_fit_gamma = {
     fit2 <- model$sample(
