62bb6c3541ee0907d24858db3cca7ed12d55d44c
Sam <s.e.abbott12@gmail.com>
2024-10-23 21:48:55 +0100

    be more careful skipping setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,15 +5,21 @@ options(datatable.print.keys = FALSE)
 if (
   !on_ci() || (on_ci() && Sys.info()["sysname"] == "Linux" && not_on_cran())
 ) {
-  library(cmdstanr) # nolint
-  temp_path <- file.path(tempdir(), "pcd_stan_functions.stan")
-  stan_functions <- pcd_load_stan_functions(
-    wrap_in_block = TRUE,
-    write_to_file = TRUE,
-    output_file = temp_path
-  )
-  model <- suppressMessages(suppressWarnings(cmdstanr::cmdstan_model( # nolint
-    temp_path
-  )))
-  model$expose_functions(global = TRUE)
+  if (requireNamespace("cmdstanr", quietly = TRUE)) { # nolint
+    if (!is.null(cmdstanr::cmdstan_version())) { # nolint
+      library(cmdstanr) # nolint
+      temp_path <- file.path(tempdir(), "pcd_stan_functions.stan")
+      stan_functions <- pcd_load_stan_functions(
+        wrap_in_block = TRUE,
+        write_to_file = TRUE,
+        output_file = temp_path
+      )
+      model <- suppressMessages(suppressWarnings(
+        cmdstanr::cmdstan_model( # nolint
+          temp_path
+        )
+      ))
+      model$expose_functions(global = TRUE)
+    }
+  }
 }
