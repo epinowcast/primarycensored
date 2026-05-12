d78d5ddbddf9887c730871017077dd3dcd9898c5
seabbs <seabbs@users.noreply.github.com>
2026-05-12 16:04:48 +0000

    deploy: 346cd4e3fa024dd137524a65f88cf528d2c5c274

--- stan/search/all_2.js
+++ stan/search/all_2.js
@@ -1,11 +1,12 @@
 var searchData=
 [
   ['delay_0',['Primary Censored Distributions Functions (Single Delay)',['../group__primary__censored__single.html',1,'']]],
-  ['dist_5flcdf_1',['dist_lcdf',['../group__delay__log__cdfs.html#ga0761ffd0ab06dc8b52d0ef30090941ce',1,'primarycensored_ode.stan']]],
-  ['distribution_2',['Log PDFs of the primary distribution',['../group__primary__distribution__log__pdfs.html',1,'']]],
-  ['distribution_20functions_3',['Exponential Growth Distribution Functions',['../group__exponential__growth__distributions.html',1,'']]],
-  ['distributions_20functions_4',['Distributions Functions',['../group__primary__event__analytical__distributions.html',1,'Analytical Solutions for Primary Event Distributions Functions'],['../group__primary__censored__distributions.html',1,'Primary Censored Distributions Functions']]],
-  ['distributions_20functions_20single_20delay_5',['Primary Censored Distributions Functions (Single Delay)',['../group__primary__censored__single.html',1,'']]],
-  ['distributions_20functions_20vectorized_6',['Primary Censored Distributions Functions (Vectorized)',['../group__primary__censored__vectorized.html',1,'']]],
-  ['documentation_20structure_7',['Documentation Structure',['../index.html#autotoc_md1',1,'']]]
+  ['dist_5fhas_5fpositive_5fsupport_1',['dist_has_positive_support',['../group__delay__log__cdfs.html#gac677200fa47a95f644537be481450960',1,'primarycensored_ode.stan']]],
+  ['dist_5flcdf_2',['dist_lcdf',['../primarycensored__ode_8stan.html#a0761ffd0ab06dc8b52d0ef30090941ce',1,'primarycensored_ode.stan']]],
+  ['distribution_3',['Log PDFs of the primary distribution',['../group__primary__distribution__log__pdfs.html',1,'']]],
+  ['distribution_20functions_4',['Exponential Growth Distribution Functions',['../group__exponential__growth__distributions.html',1,'']]],
+  ['distributions_20functions_5',['Distributions Functions',['../group__primary__event__analytical__distributions.html',1,'Analytical Solutions for Primary Event Distributions Functions'],['../group__primary__censored__distributions.html',1,'Primary Censored Distributions Functions']]],
+  ['distributions_20functions_20single_20delay_6',['Primary Censored Distributions Functions (Single Delay)',['../group__primary__censored__single.html',1,'']]],
+  ['distributions_20functions_20vectorized_7',['Primary Censored Distributions Functions (Vectorized)',['../group__primary__censored__vectorized.html',1,'']]],
+  ['documentation_20structure_8',['Documentation Structure',['../index.html#autotoc_md1',1,'']]]
 ];
--- stan/search/functions_1.js
+++ stan/search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['dist_5flcdf_0',['dist_lcdf',['../group__delay__log__cdfs.html#ga0761ffd0ab06dc8b52d0ef30090941ce',1,'primarycensored_ode.stan']]]
+  ['dist_5fhas_5fpositive_5fsupport_0',['dist_has_positive_support',['../group__delay__log__cdfs.html#gac677200fa47a95f644537be481450960',1,'primarycensored_ode.stan']]],
+  ['dist_5flcdf_1',['dist_lcdf',['../primarycensored__ode_8stan.html#a0761ffd0ab06dc8b52d0ef30090941ce',1,'primarycensored_ode.stan']]]
 ];
