b4cccd0106eb9c3af4452fec1a6b9c06ac00596c
seabbs <seabbs@users.noreply.github.com>
2026-09-10 19:59:11 +0000

    deploy: bf56c9b15ce21935726f0a710dc45b191d9eb383

--- stan/search/all_7.js
+++ stan/search/all_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['log_20cdfs_20for_20the_20ode_20function_0',['Log CDFs for the ode function',['../group__delay__log__cdfs.html',1,'']]],
   ['log_20pdfs_20of_20the_20primary_20distribution_1',['Log PDFs of the primary distribution',['../group__primary__distribution__log__pdfs.html',1,'']]],
-  ['log_5fweibull_5fg_2',['log_weibull_g',['../group__analytical__solution__helpers.html#ga2cc78a10c9ffbc81af20655c206acfc4',1,'primarycensored_analytical_cdf.stan']]]
+  ['log_5fweibull_5fg_2',['log_weibull_g',['../group__analytical__solution__helpers.html#ga2cc78a10c9ffbc81af20655c206acfc4',1,'primarycensored_analytical_cdf.stan']]],
+  ['lognormal_5flcdf_5funderflows_3',['lognormal_lcdf_underflows',['../group__delay__log__cdfs.html#ga73fe2c7117cb1128e5a863ab21317683',1,'primarycensored_ode.stan']]]
 ];
--- stan/search/functions_5.js
+++ stan/search/functions_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['log_5fweibull_5fg_0',['log_weibull_g',['../group__analytical__solution__helpers.html#ga2cc78a10c9ffbc81af20655c206acfc4',1,'primarycensored_analytical_cdf.stan']]]
+  ['log_5fweibull_5fg_0',['log_weibull_g',['../group__analytical__solution__helpers.html#ga2cc78a10c9ffbc81af20655c206acfc4',1,'primarycensored_analytical_cdf.stan']]],
+  ['lognormal_5flcdf_5funderflows_1',['lognormal_lcdf_underflows',['../group__delay__log__cdfs.html#ga73fe2c7117cb1128e5a863ab21317683',1,'primarycensored_ode.stan']]]
 ];
