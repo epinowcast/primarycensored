52d5915cddafd75035e668c38d01930e50b0af04
seabbs <seabbs@users.noreply.github.com>
2026-09-24 19:47:53 +0000

    deploy: 5e73795d6ed213f000c60ac67b060af9c9b42589

--- stan/search/all_1.js
+++ stan/search/all_1.js
@@ -5,5 +5,7 @@ var searchData=
   ['censored_20distributions_20functions_20single_20delay_2',['Primary Censored Distributions Functions (Single Delay)',['../group__primary__censored__single.html',1,'']]],
   ['censored_20distributions_20functions_20vectorized_3',['Primary Censored Distributions Functions (Vectorized)',['../group__primary__censored__vectorized.html',1,'']]],
   ['censored_20stan_20functions_4',['Primary Censored Stan Functions',['../index.html',1,'']]],
-  ['check_5ffor_5fanalytical_5',['check_for_analytical',['../group__analytical__solution__helpers.html#gafbcb39f95f9afcd6128039ece98c070a',1,'primarycensored_analytical_cdf.stan']]]
+  ['check_5ffor_5fanalytical_5',['check_for_analytical',['../group__analytical__solution__helpers.html#gafbcb39f95f9afcd6128039ece98c070a',1,'primarycensored_analytical_cdf.stan']]],
+  ['check_5ffor_5fanalytical_5fvectorized_6',['check_for_analytical_vectorized',['../group__analytical__solution__helpers.html#ga8849f96b10a4044e1e2dfbc40d2e5174',1,'primarycensored_analytical_cdf.stan']]],
+  ['check_5ffor_5funiform_5fterms_7',['check_for_uniform_terms',['../group__analytical__solution__helpers.html#ga63328e760e16acbc3020f24fd5bc50de',1,'primarycensored_analytical_cdf.stan']]]
 ];
--- stan/search/functions_0.js
+++ stan/search/functions_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['check_5ffor_5fanalytical_0',['check_for_analytical',['../group__analytical__solution__helpers.html#gafbcb39f95f9afcd6128039ece98c070a',1,'primarycensored_analytical_cdf.stan']]]
+  ['check_5ffor_5fanalytical_0',['check_for_analytical',['../group__analytical__solution__helpers.html#gafbcb39f95f9afcd6128039ece98c070a',1,'primarycensored_analytical_cdf.stan']]],
+  ['check_5ffor_5fanalytical_5fvectorized_1',['check_for_analytical_vectorized',['../group__analytical__solution__helpers.html#ga8849f96b10a4044e1e2dfbc40d2e5174',1,'primarycensored_analytical_cdf.stan']]],
+  ['check_5ffor_5funiform_5fterms_2',['check_for_uniform_terms',['../group__analytical__solution__helpers.html#ga63328e760e16acbc3020f24fd5bc50de',1,'primarycensored_analytical_cdf.stan']]]
 ];
